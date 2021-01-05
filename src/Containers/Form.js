a85f414bcbded15dfe53dd8c7d551a08f8db0a49
DCR430 <daniel.c.reyes30@gmail.com>
2021-01-05 09:33:39 -0500

    form built

--- src/Containers/Form.js
+++ src/Containers/Form.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function Form() {
-    return (
+export default class Form extends Component {
+    render() {
+        return (
         <div>
-            
+            <h2>Add a Student</h2>
+            <form>
+                Name:<input type="text"/>
+                Gif :<input type="text"/>
+             </form>
         </div>
-    )
+        )
+    }
 }
+
