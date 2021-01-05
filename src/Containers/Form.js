41e4eddd04732b80c84d874a703d29540b1b01bf
DCR430 <daniel.c.reyes30@gmail.com>
2021-01-05 10:01:43 -0500

    onChange added

--- src/Containers/Form.js
+++ src/Containers/Form.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 
 export default class Form extends Component {
+    state={
+        name:" ",
+        gif:" "
+    }
+
+    onChange=(e)=>{
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    }
+    
     render() {
         return (
         <div>
             <h2>Add a Student</h2>
             <form>
-                Name:<input type="text"/>
-                Gif :<input type="text"/>
+                Name <input name="name"
+                placeholder="student name" 
+                value={this.state.name} 
+                onChange={e => this.onChange(e)} />
+                
+                Gif  <input name="gif" 
+                placeholder="student Gif "
+                value={this.state.gif} 
+                onChange={e => this.onChange(e)} />
              </form>
         </div>
         )
